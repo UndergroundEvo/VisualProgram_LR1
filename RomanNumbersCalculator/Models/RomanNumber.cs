9a7d7d26fce6a169ec6508dda81af4bc81027bc7
Miron <popovmiron@gmail.com>
2023-02-19 17:24:29 +0700

    Добавьте файлы проекта.

---
+++ RomanNumbersCalculator/Models/RomanNumber.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumbersCalculator.Models
+{
+    public class RomanNumber : ICloneable, IComparable
+    {
+        private ushort _number;
+        private static int[] values = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        private static string[] roman = new string[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        public RomanNumber(ushort num)
+        {
+            if (num <= 0) 
+                throw new RomanNumberException($"Число {num} меньше 0.");
+            else this._number = num;
+        }
+
+        public static RomanNumber operator +(RomanNumber? num_1, RomanNumber? num_2)
+        {
+            int num = num_1._number + num_2._number;
+            if (num <= 0) 
+                throw new RomanNumberException("Не удалось сложить числа");
+            else
+                return new RomanNumber((ushort)num);
+        }
+
+        public static RomanNumber operator -(RomanNumber? num_1, RomanNumber? num_2)
+        {
+            int num = num_1._number - num_2._number;
+            if (num <= 0) 
+                throw new RomanNumberException("Результат вычитания / Результат отрицательный ");
+            else
+                return new RomanNumber((ushort)num);
+        }
+
+        public static RomanNumber operator *(RomanNumber? num_1, RomanNumber? num_2)
+        {
+            int num = num_1._number * num_2._number;
+            if (num <= 0) 
+                throw new RomanNumberException("Не удалось умножить числа");
+            else
+                return new RomanNumber((ushort)num);
+        }
+
+        public static RomanNumber operator /(RomanNumber? num_1, RomanNumber? num_2)
+        {
+            if (num_2._number == 0)
+                throw new RomanNumberException("Не удалось выполнить операцию");
+            else
+            {
+                int num = num_1._number / num_2._number;
+                if (num == 0) 
+                    throw new RomanNumberException("Не удалось поделить числа / Результат отрицательный");
+                else
+                    return new RomanNumber((ushort)num);
+            }
+        }
+        public object Clone() { return new RomanNumber(_number); }
+        public int CompareTo(object obj)
+        {
+            if (obj is RomanNumber roman)
+                return _number.CompareTo(roman._number);
+            else
+                throw new RomanNumberException("object is not a RomanNumber");
+        }
+
+        public override string ToString()
+        {
+            int tmp = _number;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < 13; i++)
+            {
+                while (tmp >= values[i])
+                {
+                    tmp -= (ushort)values[i];
+                    result.Append(roman[i]);
+                }
+            }
+            if (result.ToString() == "")
+                throw new RomanNumberException("Перевод числа в Римские цифры невозможен");
+            else
+                return result.ToString();
+        }
+    }
+}
---
+++ RomanNumbersCalculator/Models/RomanNumberExtend.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumbersCalculator.Models
+{
+    public class RomanNumberExtend : RomanNumber
+    {
+        public static ushort ToInt(string num)
+        {
+            num = new string(num.Reverse().ToArray());
+            ushort toInt = 0;
+            for (int i = 0 ; i < num.Length; i++){
+                switch (num[i]){
+                    case 'M':
+                        toInt += 1000;
+                        break;
+                    case 'D':
+                        toInt += 500;
+                        break;
+                    case 'C':
+                        toInt += 100;
+                        if (i > 0)
+                            if (num[i - 1] == 'M' || num[i - 1] == 'D')
+                                toInt -= 200;
+                        break;
+                    case 'L':
+                        toInt += 50;
+                        break;
+                    case 'X':
+                        toInt += 10;
+                        if (i > 0)
+                            if (num[i - 1] == 'C' || num[i - 1] == 'L')
+                                toInt -= 20;
+                        break;
+                    case 'V':
+                        toInt += 5;
+                        break;
+                    case 'I':
+                        toInt += 1;
+                        if (i > 0)
+                            if (num[i - 1] == 'X' || num[i - 1] == 'V')
+                                toInt -= 2;
+                        break;
+                }
+            }
+            return toInt;
+        }
+        public RomanNumberExtend(string num) : base(ToInt(num)) { }
+    }
+}
